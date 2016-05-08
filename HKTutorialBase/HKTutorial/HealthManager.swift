ef22b682a01dc814deac77af199ef388fd4689a7
Joey <joey@looker.com>
2016-05-07 19:21:35 -0700

    adding health kit base

---
+++ HKTutorialBase/HKTutorial/HealthManager.swift
@@ -0,0 +1,43 @@
+//
+//  HealthManager.swift
+//  HKTutorial
+//
+//  Created by ernesto on 18/10/14.
+//  Copyright (c) 2014 raywenderlich. All rights reserved.
+//
+
+import Foundation
+import HealthKit
+
+class HealthManager {
+  
+  let healthKitStore : HKHealthStore = HKHealthStore()
+  
+  func authorizeHealthKit(completion: ((success:Bool, error:NSError!) -> Void)!) {
+    // What we want to read
+    let healthKitTypesToRead = Set(arrayLiteral:
+      HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierStepCount)!,
+      HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierDistanceWalkingRunning)!,
+      HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierFlightsClimbed)!
+    )
+    
+    // Checking if healthkit is available
+    if !HKHealthStore.isHealthDataAvailable() {
+      let error = NSError(domain: "com.mpc.Health", code: 2, userInfo: [NSLocalizedDescriptionKey:"HealthKit is not available in this Device"])
+      if( completion != nil )
+      {
+        completion(success:false, error:error)
+      }
+      return;
+    }
+    
+    //Requesting the authorization
+    healthKitStore.requestAuthorizationToShareTypes(nil, readTypes: healthKitTypesToRead) { (success, error) -> Void in
+      if( completion != nil )
+      {
+        completion(success:success,error:error)
+      }
+    }
+  }
+  
+}
